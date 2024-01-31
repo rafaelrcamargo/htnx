8288af0f5f724f80b978e0a83b59110eb95aca47
rafaelrcamargo <rafaelrakochinski@gmail.com>
2024-01-31 16:02:48 -0300

    feat: accessibility changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,6 +40,7 @@ export default function Home() {
         <Section title="Swap outerHTML" className="col-span-2">
           <HTNX
             element="button"
+            name="Swap outerHTML button"
             action={async () => await random()}
             trigger="click"
             swap="outerHTML"
@@ -50,6 +51,7 @@ export default function Home() {
         <Section title="Swap innerHTML">
           <HTNX
             element="button"
+            name="Swap innerHTML button"
             action={async () => await like()}
             trigger="click"
             swap="innerHTML"
@@ -66,6 +68,7 @@ export default function Home() {
         <Section title="Swap outerHTML on mouseenter">
           <HTNX
             element="button"
+            name="Swap outerHTML on mouseenter button"
             action={async () => await random()}
             swap="outerHTML"
             trigger="mouseenter"
@@ -79,6 +82,7 @@ export default function Home() {
           className="col-span-2 flex-wrap">
           <HTNX
             element="button"
+            name="Swap innerHTML from target button"
             action={async () => await random()}
             trigger="click"
             target={innerRef}
@@ -98,6 +102,7 @@ export default function Home() {
         <Section title="Swap innerHTML + indicator">
           <HTNX
             element="button"
+            name="Swap innerHTML + indicator button"
             action={async () => await random({ throttle: true })}
             trigger="click"
             swap="innerHTML"
@@ -109,6 +114,7 @@ export default function Home() {
         <Section title="Swap innerHTML + loader">
           <HTNX
             element="button"
+            name="Swap innerHTML + loader button"
             trigger="click"
             swap="innerHTML"
             indicator={<Spinner />}
@@ -120,6 +126,7 @@ export default function Home() {
         <Section title="Swap innerHTML on mouseenter">
           <HTNX
             element="button"
+            name="Swap innerHTML on mouseenter button"
             action={async () => await random()}
             swap="innerHTML"
             trigger="mouseenter"
@@ -144,7 +151,10 @@ export default function Home() {
               className="lg:text-md h-16 w-64 rounded-xl border-2 border-slate-300 bg-slate-50 px-4 text-base lg:w-96 lg:text-xl dark:border-zinc-700 dark:bg-zinc-800"
             />
 
-            <button type="submit" className={BASE_BUTTON}>
+            <button
+              name="Form submit button"
+              type="submit"
+              className={BASE_BUTTON}>
               Send
             </button>
           </HTNX>
@@ -211,7 +221,7 @@ const Footer = () => (
         target="_blank"
         title="GitHub"
         href="https://github.com/rafaelrcamargo"
-        className="px-2 duration-150 first:pl-0 last:pr-0 hover:scale-110 hover:drop-shadow-md">
+        className="px-2 duration-150 first:pl-0 hover:scale-110 hover:drop-shadow-md">
         <svg width="1em" height="1em" fill="currentColor" viewBox="0 0 256 256">
           <path d="M208.31,75.68A59.78,59.78,0,0,0,202.93,28,8,8,0,0,0,196,24a59.75,59.75,0,0,0-48,24H124A59.75,59.75,0,0,0,76,24a8,8,0,0,0-6.93,4,59.78,59.78,0,0,0-5.38,47.68A58.14,58.14,0,0,0,56,104v8a56.06,56.06,0,0,0,48.44,55.47A39.8,39.8,0,0,0,96,192v8H72a24,24,0,0,1-24-24A40,40,0,0,0,8,136a8,8,0,0,0,0,16,24,24,0,0,1,24,24,40,40,0,0,0,40,40H96v16a8,8,0,0,0,16,0V192a24,24,0,0,1,48,0v40a8,8,0,0,0,16,0V192a39.8,39.8,0,0,0-8.44-24.53A56.06,56.06,0,0,0,216,112v-8A58.14,58.14,0,0,0,208.31,75.68ZM200,112a40,40,0,0,1-40,40H112a40,40,0,0,1-40-40v-8a41.74,41.74,0,0,1,6.9-22.48A8,8,0,0,0,80,73.83a43.81,43.81,0,0,1,.79-33.58,43.88,43.88,0,0,1,32.32,20.06A8,8,0,0,0,119.82,64h32.35a8,8,0,0,0,6.74-3.69,43.87,43.87,0,0,1,32.32-20.06A43.81,43.81,0,0,1,192,73.83a8.09,8.09,0,0,0,1,7.65A41.72,41.72,0,0,1,200,104Z"></path>
         </svg>
@@ -220,7 +230,7 @@ const Footer = () => (
         target="_blank"
         title="Instagram"
         href="https://instagram.com/rafaelrcmrg"
-        className="px-2 duration-150 first:pl-0 last:pr-0 hover:scale-110 hover:drop-shadow-md">
+        className="px-2 duration-150 hover:scale-110 hover:drop-shadow-md">
         <svg width="1em" height="1em" fill="currentColor" viewBox="0 0 256 256">
           <path d="M128,80a48,48,0,1,0,48,48A48.05,48.05,0,0,0,128,80Zm0,80a32,32,0,1,1,32-32A32,32,0,0,1,128,160ZM176,24H80A56.06,56.06,0,0,0,24,80v96a56.06,56.06,0,0,0,56,56h96a56.06,56.06,0,0,0,56-56V80A56.06,56.06,0,0,0,176,24Zm40,152a40,40,0,0,1-40,40H80a40,40,0,0,1-40-40V80A40,40,0,0,1,80,40h96a40,40,0,0,1,40,40ZM192,76a12,12,0,1,1-12-12A12,12,0,0,1,192,76Z"></path>
         </svg>
@@ -229,7 +239,7 @@ const Footer = () => (
         target="_blank"
         title="LinkedIn"
         href="https://linkedin.com/in/rafaelrcamargo"
-        className="px-2 duration-150 first:pl-0 last:pr-0 hover:scale-110 hover:drop-shadow-md">
+        className="px-2 duration-150 hover:scale-110 hover:drop-shadow-md">
         <svg width="1em" height="1em" fill="currentColor" viewBox="0 0 256 256">
           <path d="M216,24H40A16,16,0,0,0,24,40V216a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V40A16,16,0,0,0,216,24Zm0,192H40V40H216V216ZM96,112v64a8,8,0,0,1-16,0V112a8,8,0,0,1,16,0Zm88,28v36a8,8,0,0,1-16,0V140a20,20,0,0,0-40,0v36a8,8,0,0,1-16,0V112a8,8,0,0,1,15.79-1.78A36,36,0,0,1,184,140ZM100,84A12,12,0,1,1,88,72,12,12,0,0,1,100,84Z"></path>
         </svg>
@@ -238,7 +248,7 @@ const Footer = () => (
         target="_blank"
         title="Email"
         href="mailto:rafaelrakochinski@gmail.com"
-        className="px-2 duration-150 first:pl-0 last:pr-0 hover:scale-110 hover:drop-shadow-md">
+        className="px-2 duration-150 last:pr-0 hover:scale-110 hover:drop-shadow-md">
         <svg width="1em" height="1em" fill="currentColor" viewBox="0 0 256 256">
           <path d="M128,24a104,104,0,0,0,0,208c21.51,0,44.1-6.48,60.43-17.33a8,8,0,0,0-8.86-13.33C166,210.38,146.21,216,128,216a88,88,0,1,1,88-88c0,26.45-10.88,32-20,32s-20-5.55-20-32V88a8,8,0,0,0-16,0v4.26a48,48,0,1,0,5.93,65.1c6,12,16.35,18.64,30.07,18.64,22.54,0,36-17.94,36-48A104.11,104.11,0,0,0,128,24Zm0,136a32,32,0,1,1,32-32A32,32,0,0,1,128,160Z"></path>
         </svg>
